aaca19cc9932639829ccd5a41ea4f840680205ec
xionglei <302086963@qq.com>
2016-08-07 11:01:21 +0800

    add test2

---
+++ gittest/src/com/xl/test/Test2.java
@@ -0,0 +1,14 @@
+package com.xl.test;
+/**
+ *
+ * @author	xionglei
+ * @date 	2016年8月7日 上午10:59:29
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world test2");
+	}
+
+}
