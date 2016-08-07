0de3bb8da6926b776af039e37156cb9bbeee5d26
xionglei <302086963@qq.com>
2016-08-07 11:01:09 +0800

    update test1

--- gittest/src/com/xl/test/Test1.java
+++ gittest/src/com/xl/test/Test1.java
@@ -5,5 +5,9 @@ package com.xl.test;
  * @date 	2016年8月7日 上午10:39:54
  */
 public class Test1 {
-
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world test1");
+	}
 }
