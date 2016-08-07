8e0752a2d7fabf2baf78f9040d8aa5c0e5c33d94
xichen314159 <302086963@qq.com>
2016-08-07 23:31:04 +0800

    Update Test2.java
    update test2

--- gittest/src/com/xl/test/Test2.java
+++ gittest/src/com/xl/test/Test2.java
@@ -8,7 +8,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world test2222222222222");
+		System.out.println("hello world test2222222222222111");
 	}
 
 }
