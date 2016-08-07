8914d4be72c30c1532fd5c3650597a5cf249b7ee
xionglei <302086963@qq.com>
2016-08-07 23:35:54 +0800

    add test3

---
+++ gittest/src/com/xl/test/Test3.java
@@ -0,0 +1,15 @@
+package com.xl.test;
+/**
+ *
+ * @author	xionglei
+ * @date 	2016年8月7日 下午11:35:05
+ */
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dfgdfg");	
+		
+	}
+
+}
